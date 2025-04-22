5848c49d76de2c5971d9a895e09aeca11cade235
Serg Kuchukov Office <kuchukov.sergey@gmail.ru>
2025-04-22 17:12:14 +0300

    habit list

--- backend/habit_tracker/views.py
+++ backend/habit_tracker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListAPIView
 from habit_tracker.models import Habit
 from habit_tracker.serializer import HabitSerializer
 
@@ -7,3 +8,9 @@ class HabitViewsSet(ModelViewSet):
     #pagination_class = EducationPagination
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
+
+class HabitListViews(ListAPIView):
+    # pagination_class = EducationPagination
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+

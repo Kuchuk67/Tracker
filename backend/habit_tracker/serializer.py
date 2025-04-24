b7a2dad206323a9d1ed7ade0e27ae431a1383432
Serg <kuchukov.serg@gmail.com>
2025-04-24 07:32:06 +0300

    test

--- backend/habit_tracker/serializer.py
+++ backend/habit_tracker/serializer.py
@@ -20,3 +20,20 @@ class HabitSerializer(ModelSerializer):
                   "pablic",
                   "user", ]
         
+    def create(self, validated_data):
+        print(validated_data)
+        return Habit.objects.create(**validated_data)
+    
+    def list(self, data):
+        print('********')
+        print(self.data)
+        return Habit(self.data)
+    
+    '''def to_representation(self, instance):
+        """ Convert `username` to lowercase."""
+        print('********',instance)
+        ret = super().to_representation(instance)
+        ret['action'] = ret['action'] + " Ура !!"
+        print(ret)
+        return ret'''
+        

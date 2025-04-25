554428e8c3beaa09545d12a03caab7fbfb52e459
Serg Kuchukov Office <kuchukov.sergey@gmail.ru>
2025-04-25 18:40:39 +0300

    telegram

--- backend/telegram_bot/management/commands/telega.py
+++ backend/telegram_bot/management/commands/telega.py
@@ -2,15 +2,34 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from django.core.management.base import BaseCommand
 from users.models import CustomUser
-
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
 
 class Command(BaseCommand):
-  def handle(self, *args, **kwargs):
+
+
+    def button_bot(self, update):
+
+        keyboard = [
+            [InlineKeyboardButton("Задачи на сегодня", callback_data='1'),
+             InlineKeyboardButton("Задачи на завтра", callback_data='2')],
+            [InlineKeyboardButton("TOP возможных задач", callback_data='3')],
+        ]
+        return InlineKeyboardMarkup(keyboard)
+
+
+
+    def handle(self, *args, **kwargs):
         async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+
+
             await context.bot.send_message(chat_id=update.effective_chat.id,
-                                           text="Привет! Я бот трекер-сарвиса 'Атомарные привычки'!")
-            #print(update.effective_chat.username)
+                                           text="Привет! Я бот трекер-сарвиса 'Атомарные привычки'!"
+                                           )
+
             # Тут надо найти в БД пользователя с ником телеграма
             try:
                 user = CustomUser.objects.get(nick_telegram=update.effective_chat.username)
@@ -18,12 +37,15 @@ class Command(BaseCommand):
                 # НЕ найдено - пишем что надо зарегится на сервисе
                 await context.bot.send_message(chat_id=update.effective_chat.id,
                                                text="Не найден такой ник. Вам надо зарегится на сервисе "
-                                                    "и указать в профиле свой ник в телеге"
+                                                    "и указать в профиле свой ник в телеге",
+                                               #reply_markup=keyboard
                                                )
             else:
                 # найдено - записываем туда chat_id и пишем - ок
                 user.chat_id_telegram = update.effective_chat.id
                 user.save()
+                await update.message.reply_text('Пожалуйста, выберите:', reply_markup=self.button_bot(update))
+
 
 
         application = ApplicationBuilder().token('7507147736:AAEOYdf3erBfJ-x7unESp431YfAgufAAQ50').build()
@@ -32,3 +54,18 @@ class Command(BaseCommand):
         application.add_handler(start_handler)
 
         application.run_polling()
+
+    """async def send_welcome(message: types.Message):
+            kb = [
+                [
+                    KeyboardButton(text="Сможешь повторить это?"),
+                    KeyboardButton(text="А это?")
+                ],
+            ]
+            keyboard = ReplyKeyboardMarkup(keyboard=kb)
+
+            await message.reply(
+                "Привет!\nЯ Эхобот от Skillbox!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.",
+                reply_markup=keyboard)
+
+        application.run_polling()"""

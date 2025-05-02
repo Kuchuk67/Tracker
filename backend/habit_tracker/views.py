4335b7e1818c6f22965e0d0b482067b031eac6dd
Serg <kuchukov.serg@gmail.com>
2025-05-02 22:26:22 +0300

    docker-compose

--- backend/habit_tracker/views.py
+++ backend/habit_tracker/views.py
@@ -2,11 +2,13 @@ from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
 from habit_tracker.models import Habit
 from habit_tracker.serializer import HabitSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class HabitViewsSet(ModelViewSet):
     # pagination_class = EducationPagination
     serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         """
@@ -24,3 +26,4 @@ class HabitListViews(ListAPIView):
     # pagination_class = EducationPagination
     serializer_class = HabitSerializer
     queryset = Habit.objects.filter(public=True)
+    permission_classes = [IsAuthenticated]
